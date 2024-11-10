965f9f7ba62d5869272a4665b3aa411bb909ac1e
Shunsuke Hochu <124444636+itukahashinu@users.noreply.github.com>
2024-11-11 01:03:31 +0900

    django basic mechanism

--- backend/mysite/polls/views.py
+++ backend/mysite/polls/views.py
@@ -1,5 +1,44 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
+from .models import Question, Choice
+from django.http import Http404
+
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
+
+def results(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/results.html", {"question": question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # 選択肢が選ばれていない場合、エラーメッセージを表示する
+        return render(request, 'polls/vote.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        # 投票数を増加し、結果ページにリダイレクトする
+        selected_choice.votes += 1
+        selected_choice.save()
+        # リダイレクトを使って、ユーザーが二重送信するのを防ぐ
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
