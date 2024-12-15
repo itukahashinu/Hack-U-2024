2005548f7703ed581635b659e1742d573724fabd
Shunsuke Hochu <124444636+itukahashinu@users.noreply.github.com>
2024-12-15 22:14:59 +0900

    デフォルトテーマに関する修正

--- backend/mysite/contest/models.py
+++ backend/mysite/contest/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
+from django.core.cache import cache
 
+# デフォルトテーマ取得用の関数
+def get_default_theme():
+    default_theme = cache.get('default_theme')
+    if not default_theme:
+        default_theme, created = Theme.objects.get_or_create(
+            title='デフォルトテーマ',
+            description='デフォルトテーマの説明',
+            start_date=datetime(2020, 1, 1),
+            end_date=datetime(2050, 12, 31),
+        )
+        cache.set('default_theme', default_theme)
+    return default_theme
+
+# テーマモデル
 class Theme(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
@@ -10,18 +26,9 @@ class Theme(models.Model):
     def __str__(self):
         return self.title
 
-    @classmethod
-    def get_default_theme(cls):
-        default_theme, created = cls.objects.get_or_create(
-            title='デフォルトテーマ',
-            description='デフォルトテーマの説明',
-            start_date='2020-01-01',
-            end_date='2050-12-31',
-        )
-        return default_theme
-
+# アイデアモデル
 class Idea(models.Model):
-    theme = models.ForeignKey(Theme, on_delete=models.CASCADE, default=Theme.get_default_theme)
+    theme = models.ForeignKey(Theme, on_delete=models.CASCADE, default=get_default_theme)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
