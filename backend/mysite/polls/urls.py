d0718b13ca7587aefa4ceb9731e759678bc5d7fa
yuto52 <221205013@ccmailg.meijo-u.ac.jp>
2024-12-19 00:10:15 +0900

    Reapply "Merge pull request #8 from itukahashinu/revert-7-advertisement_yuto"
    This reverts commit b335b9ad400a799e78119846771e167487aedee0.

--- backend/mysite/polls/urls.py
+++ backend/mysite/polls/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from polls import views
-
+from .views import get_active_surveys
 
 
 app_name = 'polls'
@@ -18,11 +18,8 @@ main_urlpatterns = [
     path('surveys/<int:survey_id>/response/', views.survey_response_view, name='survey_response'),
     path('survey/<int:survey_id>/update-status/', views.update_survey_status, name='update_survey_status'),
     path('survey/<int:survey_id>/submit/', views.submit_survey, name='submit_survey'),
-
-    path('get_active_surveys/', views.get_active_surveys, name='get_active_surveys'),
-    path('submit_survey_response/<int:survey_id>/', views.submit_survey, name='submit_survey_response'),
-
-
+    path('surveys/active/', views.get_active_surveys, name='active_surveys'),
+    path('surveys/<int:survey_id>/results/', views.survey_results, name='survey_results'),
 ]
 # API用URL
 api_urlpatterns = [
